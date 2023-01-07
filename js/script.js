ec463c7676dc6732e3f9e8714a6271d194fd9b54
Daniel Nunes <dani.perfil.profissional@gmail.com>
2023-01-07 15:28:34 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const header = document.querySelector('header');
 const menu = document.querySelector('.menu');
+const url = 'https://api.shrtco.de/v2/';
 
 menu.addEventListener('click', e => {
   header.classList.toggle('ativo');
 });
+
+async function getLink() {
+  const items = await fetch(url);
+  console.log(items);
+}
+getLink();
