741a3355ddf511acd3f543ea7768a13d69956c8a
Daniel Nunes <dani.perfil.profissional@gmail.com>
2023-01-18 22:42:29 -0300

    first version of API challenge

--- js/script.js
+++ js/script.js
@@ -1,13 +1,50 @@
 const header = document.querySelector('header');
 const menu = document.querySelector('.menu');
-const url = 'https://api.shrtco.de/v2/';
+const linkShort = document.querySelector('.addLink');
+const addLink = document.querySelector('.links-content');
+const input = linkShort.querySelector('input');
 
-menu.addEventListener('click', e => {
-  header.classList.toggle('ativo');
+let shortenLink = null;
+
+const linkShortCut = async link => {
+  const url = await fetch(`https://api.shrtco.de/v2/shorten?url=${link}`);
+  const data = await url.json();
+  shortenLink = await data.result.full_short_link;
+  return shortenLink;
+};
+
+const submitLink = async event => {
+  event.preventDefault();
+
+  if (input.value) {
+    addLinkShort(input.value, await linkShortCut(input.value));
+    input.value = '';
+    return;
+  }
+};
+
+const addLinkShort = async (link, shortLink) => {
+
+  addLink.innerHTML += ` <div class="newlink-copy">
+  <p class="link-to-generate">${link}</p>
+<div class="generate-link-content">
+<a href="#" class="new-link">${shortLink}</a>
+<button class="button-copy">Copy</button>
+</div>
+</div>`;
+};
+
+addLink.addEventListener('click', async e => {
+  if (e.target.textContent === 'Copy') {
+    await navigator.clipboard.writeText(shortenLink);
+    e.target.textContent = 'Copied';
+  }
 });
 
-async function getLink() {
-  const items = await fetch(url);
-  console.log(items);
-}
-getLink();
+// button.addEventListener('click', e => {
+//   button.textContent('Copied!');
+//   navigator.clipboard.writeText(linkShortCut);
+// });
+
+menu.addEventListener('click', e => header.classList.toggle('ativo'));
+linkShort.addEventListener('submit', submitLink);
